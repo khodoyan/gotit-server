d2a61aea38a53a2e1ad3f18fd5c9ec0af13a8699
eduardkhodoyan <eduard.khodoyan@gmail.com>
2015-10-22 14:45:45 +0200

    Added custom 404 error page

--- src/main/java/pro/khodoian/GotitServerApplication.java
+++ src/main/java/pro/khodoian/GotitServerApplication.java
@@ -1,15 +1,28 @@
 package pro.khodoian;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class GotitServerApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(GotitServerApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+            }
+        };
     }
 
 }
